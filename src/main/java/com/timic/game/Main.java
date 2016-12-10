f92b5ec257ef4a7aa4628ff1e078708ecd11690e
Timotej <timotej.manfreda@gmail.com>
2016-12-10 18:39:06 +0100

    I prefer pink more...

--- src/main/java/com/timic/game/Main.java
+++ src/main/java/com/timic/game/Main.java
@@ -117,7 +117,7 @@ public class Main {
     private void loop() {
         GL.createCapabilities();
 
-        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+        glClearColor(1.0f, 0.0f, 1.0f, 0.0f);
 
         while(!glfwWindowShouldClose(window)) {
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
