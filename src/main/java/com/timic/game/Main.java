8dcecf26cff773de29e4efd4a2d67d5170352f03
Timotej <timotej.manfreda@gmail.com>
2016-12-10 16:05:52 +0100

    Initial commit

---
+++ src/main/java/com/timic/game/Main.java
@@ -0,0 +1,84 @@
+package com.timic.game;
+
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+public class Main {
+
+    private long window;
+
+    private void run() {
+        System.out.println("Running LWJGL " + Version.getVersion());
+
+        try {
+            init();
+            loop();
+
+            glfwFreeCallbacks(window);
+            glfwDestroyWindow(window);
+        } finally {
+            glfwTerminate();
+            glfwSetErrorCallback(null).free();
+        }
+    }
+
+    private void init() {
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        if(!glfwInit())
+            throw new IllegalStateException("Unable to initialize GLFW!");
+
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        // TODO: Make them constants
+        int WIDTH = 1270;
+        int HEIGHT = 768;
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "gEngine v1.1", NULL, NULL);
+        if(window == NULL)
+            throw new RuntimeException("Failed to create the GLFW window!");
+
+        glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+            if(key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+                glfwSetWindowShouldClose(window, true);
+        });
+
+        GLFWVidMode vidMode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+        glfwSetWindowPos(
+                window,
+                (vidMode.width()-WIDTH)/2,
+                (vidMode.height()-HEIGHT)/2
+        );
+
+        glfwMakeContextCurrent(window);
+        glfwSwapInterval(1);
+
+        glfwShowWindow(window);
+    }
+
+    private void loop() {
+        GL.createCapabilities();
+
+        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+
+        while(!glfwWindowShouldClose(window)) {
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Main().run();
+    }
+}
